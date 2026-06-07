a772c4f800779670a69c8712e0397849029938e7
PhungVanCuong <cuongphung1810@gmail.com>
2026-06-07 14:36:10 +0700

    123

--- src/components/Admin/DangNhap/index.vue
+++ src/components/Admin/DangNhap/index.vue
@@ -20,7 +20,7 @@
                     <div class="w-100">
                         
                         <div class="d-flex justify-content-center align-items-center gap-2 d-lg-none mb-4">
-                            <img src="" alt="Logo" class="img-fluid" style="max-width: 60px;">
+                            <img src="https://logo.com/image-cdn/images/kts928pd/production/2422e0e54bc42735594cd2f3800bdd15ce7ab258-317x316.png?w=1080&q=72&fm=webp" alt="Logo" class="img-fluid" style="max-width: 60px;">
                             <h2 class="fw-bold text-dark m-0">IXTAL</h2>
                         </div>
 
